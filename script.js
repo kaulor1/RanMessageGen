5cb9c0206e8affcd09cbb8c9eb61c5c8cdd30863
Jeremiah Daemien Stone <jprimethetiger@gmail.com>
2024-10-03 11:07:53 -0500

    added message array and the function generateMesasge

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello Peeps of the World");
+const messages = [
+  "Love all, trust a few, do wrong to none",
+  "Follow the Wind, you will never be lost",
+  "We can only learn to love by loving",
+  "Life is like a Sakura Fubuki fleeting yet beautiful ",
+  "Life lived best is one that is loved",
+];
+
+const generateMessage = () => {
+  const randomIndex = Math.floor(Math.random() * messages.length);
+  randomMessages = messages[randomIndex];
+  console.log(randomMessages);
+};
+
+generateMessage();
